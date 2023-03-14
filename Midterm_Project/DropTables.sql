52e771bcd67aa4496f64573c15ee88f05b4530bc
grapemoli <105399768+grapemoli@users.noreply.github.com>
2023-03-14 18:39:14 -0400

    Update DropTables.sql

--- Midterm_Project/DropTables.sql
+++ Midterm_Project/DropTables.sql
@@ -1,38 +1,57 @@
-/*
+/************************
 File Name: DropTables.sql
 By: Grace Nguyen
-*/
--- 10. ROOM_UPKEEP Table
+Purpose: (PART 1) DROP ALL TABLES, CONSTRAINTS, INDICES, ETC.
+*************************/
+--Drop Indices!
+drop index CUSTOMER$FirstName_LastName;
+drop index TRANSACTION$CustomerID_Room;  
+drop index EMPLOYEE$FirstName_LastName;   --#18 in rubric
+
+-- 12. MEMBER table
+alter table MEMBER drop constraint FK_Member_Customer
+alter table MEMBER drop constraint PK_Member;
+drop sequence MemberID;
+drop table MEMBER;
+
+-- 11. ROOM_UPKEEP Table
 alter table ROOM_UPKEEP drop constraint FK_RoomUpkeep_Employee;
 alter table ROOM_UPKEEP drop constraint FK_RoomUpkeep_Rate;
 drop table ROOM_UPKEEP;
 
--- 9. EMPLOYEE Table
+-- 10. EMPLOYEE Table
 alter table EMPLOYEE drop constraint FK_Employee_Location;
 alter table EMPLOYEE drop constraint FK_Employee_Department;
 alter table EMPLOYEE drop constraint FK_Employee_Position;
 drop table EMPLOYEE;
 drop sequence EmployeeID;
 
--- 8. DEPARTMENT Table
+-- 9. DEPARTMENT Table
+alter table DEPARTMENT drop constraint DEPARTMENT_UQ;
 drop table DEPARTMENT;
 drop sequence DeptID;
 
--- 7. POSITION Table
+-- 8. POSITION Table
+alter table POSITION drop constraint POSITION_UQ;
 drop table POSITION;
 drop sequence PositionID;
 
--- 6. TRANSACTION Table
+-- 7. TRANSACTION Table
 alter table TRANSACTION drop constraint FK_Transaction_Customer;
 alter table TRANSACTION drop constraint FK_Transaction_Room;
 alter table TRANSACTION drop constraint FK_Transaction_Rate;
 drop table TRANSACTION;
 drop sequence TransactionID;
 
--- 5. ROOM Table
+-- 6. ROOM Table
 drop table ROOM;
 
+--5. ROOM_RATE Table
+alter table ROOM_RATE drop constraint ROOMTYPE_UQ;
+drop table ROOM_TYPE;
+
 -- 4. RATE Table
+alter table RATE drop constraint RATE_UQ;
 drop table RATE;
 
 -- 3. CUSTOMER Table
@@ -46,4 +65,5 @@ drop table LOCATION;
 drop sequence LocationID;
 
 -- 1. STATE Table
+alter table STATE drop constraint STATE_UQ;
 drop table STATE;
